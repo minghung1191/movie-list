8402d6073b05145f1846c11ffc9f9220ade8036d
minghung1191 <minghung1191@gmail.com>
2024-06-20 20:23:58 +0800

    build: route & controller setup

--- app.js
+++ app.js
@@ -3,7 +3,16 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('express app for movies')
+    res.redirect('/movies') //直接導向movies頁面
+})
+
+app.get('/movies', (req, res) => {
+    res.send('listing movies')
+})
+
+app.get('/movie/:id', (req, res) => {
+    const id = req.params.id
+    res.send(`reading movie: ${id}`)
 })
 
 app.listen(port, () => {
