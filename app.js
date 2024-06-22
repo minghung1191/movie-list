4872a534c7efc881d4a05302aeb4637f8749d3a2
minghung1191 <minghung1191@gmail.com>
2024-06-22 16:28:49 +0800

    feat: add listing page & change bootstrap css

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require('express')
 const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
+const movies = require('./public/jsons/movies.json').results
+const BASE_IMG_URL ='https://movie-list.alphacamp.io/posters/'
 
 app.engine('.hbs', engine({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 app.set('views', './views');
-
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
@@ -14,7 +15,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/movies', (req, res) => {
-    res.render('index')
+    res.render('index', { movies, BASE_IMG_URL })
 })
 
 app.get('/movie/:id', (req, res) => {
